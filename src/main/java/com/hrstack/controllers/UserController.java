ecbda29b5642e38275d7298e656526cbaa0864ae
macbook <graceolubiyi@gmail.com>
2026-07-01 09:59:56 +0100

    wip

--- src/main/java/com/hrstack/controllers/UserController.java
+++ src/main/java/com/hrstack/controllers/UserController.java
@@ -15,7 +15,6 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping("/api/v1/auth")
 @RequiredArgsConstructor
 public class UserController {
-
     private final UserService userService;
     private final OtpService otpService;
 
