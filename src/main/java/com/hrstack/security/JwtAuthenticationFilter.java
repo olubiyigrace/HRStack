9d7c01af7d9925b7041ca6ad261a73968641833c
macbook <graceolubiyi@gmail.com>
2026-07-02 09:46:48 +0100

    wip

--- src/main/java/com/hrstack/security/JwtAuthenticationFilter.java
+++ src/main/java/com/hrstack/security/JwtAuthenticationFilter.java
@@ -26,7 +26,6 @@ import java.util.Collections;
 @RequiredArgsConstructor
 @Slf4j
 public class JwtAuthenticationFilter extends OncePerRequestFilter {
-
     private final JwtService jwtService;
     private final RedisSessionService redisSessionService;
 
